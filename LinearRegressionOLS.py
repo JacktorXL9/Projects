9fe53f63b88cd41d0aa582a00b405259cfd16a35
Spencer Percy <spenperc@gmail.com>
2019-06-11 15:14:57 -0400

    A working model for future use

--- LinearRegressionOLS.py
+++ LinearRegressionOLS.py
@@ -1,9 +1,10 @@
-import numpy as np 
+import numpy as np
+from numpy.linalg import inv 
 """For the purposes of developing this from the ground up we are going to be
    building this assuming that we are inputting the values 
    directly into the file"""
-"""Here we are being fed the dimensions of the array where 'n' is the number of
-   features and 'm' is the number of data points"""
+"""Here we are being fed the dimensions of the array where 'm' is the number of
+   features and 'n' is the number of data points"""
 print("Please input first number of features and then number of rows of data")
 m,n = [int(i) for i in input().strip().split(' ')]
 """Here we are creating two empty lists to store our variables"""
@@ -14,10 +15,21 @@ Y = []
    X11   X21  Y1
    X12   X22  Y2"""
 for i in range(n):
-    print("Please input row {}".format(i))
+    print("Please input row {}".format(i+1))
     data = input().strip().split(' ')
     X.append(data[:m])
     Y.append(data[m:])
-print(m,n)
-print(X)
-print(Y)
+X = np.array(X,float)
+Y = np.array(Y,float)
+X = np.c_[ np.ones(n), X]
+Beta = np.dot(np.dot(inv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
+print('Please input your {} data points'.format(m))
+X_input = []
+data = input().strip().split(' ')
+X_input.append(data)
+X_input = np.array(X_input,float)
+X_input = np.c_[ np.ones(1), X_input]
+Y_new = np.dot(X_input, Beta)
+
+
+print(Y_new)
